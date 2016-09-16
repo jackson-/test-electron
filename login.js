49b0952d8134513a1d766ee874b27f2f97113b48
Devin "GIT SMASH!" Jackson <jackson.t.devin@gmail.com>
2016-09-15 22:35:31 -0400

    init commit

---
+++ login.js
@@ -0,0 +1,7 @@
+const remote = require('electron').remote
+const main = remote.require('./main.js')
+
+var button = document.getElementById('login_button')
+button.addEventListener('click', () => {
+    main.open-url(event, `file://${__dirname}/form_component.html`)
+}, false)
