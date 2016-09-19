503d3c4307af16e8ad2a3edeeebc38ea1fdbddde
Devin "GIT SMASH!" Jackson <jackson.t.devin@gmail.com>
2016-09-19 09:54:28 -0400

    css method, rethinkdb
    added css sample parse in index.js and installed rethinkdb

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 const remote = require('electron').remote
 const main = remote.require('./main.js')
+const r = require('rethinkdb')
+var fs = require('fs');
+var parse = require('csv-parse');
+var conn = r.connect({db: 'marvel'});
+
+var csvData=[];
+fs.createReadStream('./assets/aqua.csv')
+    .pipe(parse({delimiter: ':'}))
+    .on('data', function(csvrow) {
+        //do something with csvrow
+        csvData.push(csvrow);
+    })
+    .on('end',function() {
+      //do something with csvData
+      r.table("aqua").insert(csvData).run(conn)
+      console.log(csvData);
+    });
--- login.js
+++ login.js
@@ -3,5 +3,6 @@ const main = remote.require('./main.js')
 
 var button = document.getElementById('login_button')
 button.addEventListener('click', () => {
+    event.preventDefault();
     main.open-url(event, `file://${__dirname}/form_component.html`)
 }, false)
