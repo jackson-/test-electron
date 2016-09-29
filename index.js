822fc3dbeca2c714405c0f741b5577f28948bdcf
Devin "GIT SMASH!" Jackson <jackson.t.devin@gmail.com>
2016-09-28 21:04:59 -0400

    updates
    added rethinkdb
    trying to put csv in db

--- index.js
+++ index.js
@@ -3,17 +3,22 @@ const main = remote.require('./main.js')
 const r = require('rethinkdb')
 var fs = require('fs');
 var parse = require('csv-parse');
-var conn = r.connect({db: 'marvel'});
-
 var csvData=[];
-fs.createReadStream('./assets/aqua.csv')
-    .pipe(parse({delimiter: ':'}))
-    .on('data', function(csvrow) {
-        //do something with csvrow
-        csvData.push(csvrow);
-    })
-    .on('end',function() {
-      //do something with csvData
-      r.table("aqua").insert(csvData).run(conn)
-      console.log(csvData);
-    });
+//Converter Class
+var Converter = require("csvtojson").Converter;
+var converter = new Converter({});
+
+//end_parsed will be emitted once parsing finished
+converter.on("end_parsed", function (jsonArray) {
+   console.log(jsonArray); //here is your result jsonarray
+});
+
+//read from file
+var csvData = require("fs").createReadStream("./assets/aqua.csv").pipe(converter);
+
+console.log(csvData);
+r.connect({host: 'localhost', db: 'test'}, function(err, conn){
+    r.table('aqua').insert(csvData).run(conn);
+    r.db('test').table('aqua').run(conn)
+});
+console.log("DONE")
